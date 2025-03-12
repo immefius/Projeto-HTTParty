8b07e9bc96695f1416ce04b9ca8b7b5be8e65260
Matheus Rodrigues <matheus.rodrigues1228@gmail.com>
2025-03-12 06:04:21 -0300

    post no httparty

--- features/bdd/get.feature
+++ features/bdd/get.feature
@@ -9,3 +9,9 @@ Funcionalidade: Pesquisar Funcionarios
         Dado que o usuario consulte informacoes do funcionario
         Quando ele realizar a busca
         Entao uma lista de funcionarios deve retornar
+
+    @cenario_dois
+    Cenario: Cadastrar novo funcionario
+        Dado que o usuario queira cadastrar um novo funcionario
+        Quando ele inserir seus dados
+        Entao o funcionario estara cadastrado
--- features/step_definitions/get.rb
+++ features/step_definitions/get.rb
@@ -10,3 +10,25 @@ Dado('que o usuario consulte informacoes do funcionario') do
     expect(@list_employee.code).to eql 200
     expect(@list_employee.message).to eql 'OK'
   end
+
+  Dado('que o usuario queira cadastrar um novo funcionario') do
+    @post_url = 'url da api'
+  end
+
+    Quando ('ele inserir seus dados') do
+      @create_employee = HTTParty.post(@post_url, :headers => {'Content-Type': 'application/json'}, body: {
+        "employee_name": "nome do funcionario",
+        "employee_salary": 000000,
+        "employee_age": 00,
+        "profile_image": ""
+      }.to_json)
+    end
+
+    Entao('o funcionario estara cadastrado') do
+      expect(@create_employee.code).to eql (200)
+      expect(@create_employee.msg).to eql 'OK'
+      expect(@create_employee["status"]).to eql 'success'
+      expect(@create_employee["message"]).to eql 'Successfully! Record has been added.'
+      expect(@create_employee['data']["employee_name"]).to eql 'nome do funcionario'
+      expect(@create_employee['data']["employee_salary"]).to eql (000000)
+      expect(@create_employee['data']["employee_age"]).to eql (00)

f26e731da06c7a0ccb10fe82d0ec43d4a7e2d004
Matheus Rodrigues <matheus.rodrigues1228@gmail.com>
2025-03-13 06:02:49 -0300

    comandos put e delete

--- features/bdd/employee.feature
+++ features/bdd/employee.feature
@@ -15,3 +15,15 @@ Funcionalidade: Pesquisar Funcionarios
         Dado que o usuario queira cadastrar um novo funcionario
         Quando ele inserir seus dados
         Entao o funcionario estara cadastrado
+
+    @cenario_tres
+    Cenario: Alterar informacoes cadastrais
+        Dado que o usuario queira alterar alguma informacao
+        Quando ele enviar as alteracoes
+        Entao novas informacoes serao definidas
+
+    @cenario_quatro
+    Cenario: Deletar cadastro de funcionario
+        Dado que o usuario queira deletar um funcionario
+        Quando ele enviar a identificacao unica
+        Entao esse funcionario sera deletado do sistema
--- features/step_definitions/employee.rb
+++ features/step_definitions/employee.rb
@@ -32,3 +32,45 @@ Dado('que o usuario consulte informacoes do funcionario') do
       expect(@create_employee['data']["employee_name"]).to eql 'nome do funcionario'
       expect(@create_employee['data']["employee_salary"]).to eql (000000)
       expect(@create_employee['data']["employee_age"]).to eql (00)
+
+
+    Dado('que o usuario queira alterar alguma informacao') do
+      @put_url = 'url do metodo put com ID do campo a ser alterado'
+    end
+
+      Quando('ele enviar as alteracoes') do
+        @update_employee = HTTParty.put(@put_url, :headers => {'Content-Type': 'application/json'}, body: {
+          "employee_name": "nome do funcionario",
+          "employee_salary": 000000,
+          "employee_age": 00,
+          "profile_image": ""
+        }.to_json)
+      end
+
+      Entao('novas informacoes serao definidas') do 
+        expect(@update_employee.code).to eql (200)
+        expect(@update_employee.msg).to eql 'OK'
+        expect(@update_employee["status"]).to eql 'success'
+        expect(@update_employee["message"]).to eql 'Successfully! Record has been updated.'
+        expect(@update_employee['data']["employee_name"]).to eql 'nome do funcionario'
+        expect(@update_employee['data']["employee_salary"]).to eql (000000)
+        expect(@update_employee['data']["employee_age"]).to eql (00)
+      end
+
+
+Dado('que o usuario queira deletar um funcionario') do
+  @get_employee = HTTParty.get('url da api', :headers => {'Content-Type': 'application/json'})
+  @delete_url = 'url do metodo DELETE' + @get_employee['data'][0]['id'].to_s
+end
+
+  Quando('ele enviar a identificacao unica') do
+    @delete_employee = HTTParty.delete(@delete_url, :headers => {'Content-Type': 'application/json'})
+  end
+
+  Entao('esse funcionario sera deletado do sistema') do
+    expect(@delete_employee.code).to eql (200)
+    expect(@delete_employee.msg).to eql 'OK'
+    expect(@delete_employee["status"]).to eql 'success'
+    expect(@delete_employee["data"]).to eql 'id do funcionario'
+    expect(@delete_employee["message"]).to eql 'Successfully! Record has been deleted'
+  end
